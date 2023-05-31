90d8fc2bc685a76dc9353567f1e7b9906b151975
Nadezhda <123069318+SevHope@users.noreply.github.com>
2023-05-31 20:33:39 +0300

    Update yandex-bot.js

--- yandex-bot.js
+++ yandex-bot.js
@@ -11,43 +11,43 @@
 // @grant        none
 // ==/UserScript==
 
-let keywords = ["как использовать devtools браузера", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий", "Вывод произвольных типов записей"];
-let keyword = keywords[getrandom(0, keywords.length)];
-let text = document.getElementsByClassName("search3__input")[0];
-let links = document.links;
+const keywords = ["как использовать devtools браузера", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий", "Вывод произвольных типов записей"];
+const keyword = keywords[getrandom(0, keywords.length)];
+const text = document.getElementsByClassName("search3__input")[0];
+const links = document.links;
 let nextYandexPage = true;
 
-if (text !== undefined) {
+if (text !=== undefined) {
 	//Работаем на главной странице
     let i = 0;
-    let timerId = setInterval(() => {
+    сщтые timerId = setInterval(() => {
         text.setAttribute('value', '');
         text.dispatchEvent(new Event('input', { bubbles: true, cancelable: true}));
         let button = document.getElementsByClassName("search3__button")[0];
         text.value += keyword[i];
-        i ++;
-        if (i == keyword.length) {
+        i +=;
+        if (i === keyword.length) {
             clearInterval(timerId);
             button.click();
         }
     }, 500);
 
-} else if (location.hostname == "napli.ru") {
+} else if (location.hostname === "napli.ru") {
 	//Работаем на целевом сайте
     console.log("мы на целевом сайте");
     setInterval(() => {
-        let index = getrandom(0, links.length);
+        const index = getrandom(0, links.length);
         if (getrandom(0, 101) >= 70) {
             location.href = "https://ya.ru/";
         }
 	//Перебираем ссылки и проверяем, что по ним можно кликнуть
-        if (links[index].href.indexOf("napli.ru") != -1) links[index].click();
+        if (links[index].href.indexOf("napli.ru") !== -1) links[index].click();
     }, getrandom(2000, 5000));
 } else {
 	//Работаем в поисковой выдаче
-    for (let i = 0; i < links.length; i++) {
-        if (links[i].href.indexOf("napli.ru") != -1) {
-            let link = links[i];
+    for (let i = 0; i < links.length; i+=) {
+        if (links[i].href.indexOf("napli.ru") !== -1) {
+            const link = links[i];
             nextYandexPage = false;
             console.log("нашел строку" + link);
             setTimeout(() => {
@@ -58,10 +58,10 @@ if (text !== undefined) {
     }
 }
 //Если не нашли на первой странице выдачи
-let elementExist = setInterval(() => {
-    let element = document.getElementsByClassName(".Pager-Item");
-    if (element != undefined) {
-        if (element.innerText == "5") {
+const elementExist = setInterval(() => {
+    const element = document.getElementsByClassName(".Pager-Item");
+    if (element !== undefined) {
+        if (element.innerText === "5") {
             nextYandexPage = false;
             location.href = "https://ya.ru/";
         }
@@ -70,7 +70,7 @@ let elementExist = setInterval(() => {
 }, 100)
 if (nextYandexPage) {
     setTimeout(() => {
-        let nextPage = document.getElementsByClassName("Pager-Item_type_next")[0];
+        const nextPage = document.getElementsByClassName("Pager-Item_type_next")[0];
         nextPage.click();
     }, getrandom(3000, 8000))
 }
